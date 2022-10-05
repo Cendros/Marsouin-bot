f4553abe45054142e0a9c242fdff0595b43e8302
Cendros <ludorjt12@gmail.com>
2022-10-05 19:39:32 +0200

    ready for release

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config();
-}
+require('dotenv').config();
 
 const fs = require('node:fs');
 
